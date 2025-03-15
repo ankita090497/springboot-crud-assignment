8527dceac8e87217a880ea49c9cd5a10808fcd8f
ankita090497 <ankita090497@gmail.com>
2025-03-15 09:41:25 +0530

    Person : Exception classes created for global exception handling

---
+++ Person/src/main/java/com/person/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.person.exception;
+
+public class ResourceNotFoundException extends RuntimeException  {
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
